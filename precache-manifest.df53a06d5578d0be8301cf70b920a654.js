302ab9570ff1f4696ecc7eb005fad09fad9536d2
Marco Abate <m.abate@logispin.com>
2020-05-05 21:34:11 +0200

    Updates

---
+++ precache-manifest.df53a06d5578d0be8301cf70b920a654.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21090e73740bfbbeb55eaabbf69c957e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "482ccac58872fe4a8a96",
+    "url": "/static/css/2.e74a7d18.chunk.css"
+  },
+  {
+    "revision": "cb66d9ffa588676c5429",
+    "url": "/static/css/main.70485378.chunk.css"
+  },
+  {
+    "revision": "482ccac58872fe4a8a96",
+    "url": "/static/js/2.300ef64b.chunk.js"
+  },
+  {
+    "revision": "4ba1a1c11c57ed15aa68008707a3d6b4",
+    "url": "/static/js/2.300ef64b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cb66d9ffa588676c5429",
+    "url": "/static/js/main.53749004.chunk.js"
+  },
+  {
+    "revision": "ec3ba461a26e0c1f9ac3",
+    "url": "/static/js/runtime-main.4a903731.js"
+  },
+  {
+    "revision": "1fa4147b0544c87a8933322999a887c5",
+    "url": "/static/media/BrandonGrotesque-Black.1fa4147b.woff2"
+  },
+  {
+    "revision": "33bc2fdf69d61c0abd8f43d41c995e87",
+    "url": "/static/media/BrandonGrotesque-Black.33bc2fdf.woff"
+  },
+  {
+    "revision": "3bebd53c0ee89703d8829e97e2ca0784",
+    "url": "/static/media/BrandonGrotesque-Black.3bebd53c.ttf"
+  },
+  {
+    "revision": "0b59275f161f6f43f0ca3b2365a77f77",
+    "url": "/static/media/BrandonGrotesque-Bold.0b59275f.ttf"
+  },
+  {
+    "revision": "1ad8d40bebcdcc1f79f0e22ce35b3d75",
+    "url": "/static/media/BrandonGrotesque-Bold.1ad8d40b.woff"
+  },
+  {
+    "revision": "441b33aac91c0f5883e390134d6fa6eb",
+    "url": "/static/media/BrandonGrotesque-Bold.441b33aa.woff2"
+  },
+  {
+    "revision": "08ee9db6dc65093bb66dbaae7094f087",
+    "url": "/static/media/BrandonGrotesque-Medium.08ee9db6.woff2"
+  },
+  {
+    "revision": "920c11f315161dc7ff0204c5502105e2",
+    "url": "/static/media/BrandonGrotesque-Medium.920c11f3.woff"
+  },
+  {
+    "revision": "ef4fddb0ac2f5cdf0b2eb54bf9898fb0",
+    "url": "/static/media/BrandonGrotesque-Medium.ef4fddb0.ttf"
+  },
+  {
+    "revision": "013a89eb356fb88baad2f6178c5400ba",
+    "url": "/static/media/BrandonGrotesque-Regular.013a89eb.woff"
+  },
+  {
+    "revision": "aba7d12553de107511905eb7595a72d9",
+    "url": "/static/media/BrandonGrotesque-Regular.aba7d125.ttf"
+  },
+  {
+    "revision": "aff015dfe96d4c9021ace79558be225d",
+    "url": "/static/media/BrandonGrotesque-Regular.aff015df.woff2"
+  },
+  {
+    "revision": "10cc8d8755a6ebdc69c8e4fa006de6fc",
+    "url": "/static/media/BrandonGrotesque-RegularItalic.10cc8d87.woff"
+  },
+  {
+    "revision": "5267a22825e445ebb40686639b9c1cd8",
+    "url": "/static/media/BrandonGrotesque-RegularItalic.5267a228.woff2"
+  },
+  {
+    "revision": "ba6abfddb0c1143b43d41ce9bc6fc236",
+    "url": "/static/media/BrandonGrotesque-RegularItalic.ba6abfdd.ttf"
+  }
+]);
